90fde38dcdb7021c06183fe699670b3db00004df
cysong <cysong@coinone.com>
2024-07-14 14:29:12 +0900

    chore: container init.sql query 수정

--- .container/mysql/initdb.d/2-init.sql
+++ .container/mysql/initdb.d/2-init.sql
@@ -1,4 +1,4 @@
-INSERT INTO blockchain_network (id, network, block_number) values (1, 'EVM Sidechain', 0);
-INSERT INTO blockchain_network (id, network, block_number) values (2, 'XRP', 0);
-INSERT INTO blockchain_bridge_token (network_id, symbol, contract_address) values (1, 'eXRP', 0);
-INSERT INTO blockchain_bridge_token (network_id, symbol, contract_address) values (2. 'XRP', 0);
+INSERT INTO blockchain_network (id, network, block_number) VALUES (1, 'EVM Sidechain', 0);
+INSERT INTO blockchain_network (id, network, block_number) VALUES (2, 'XRP', 0);
+INSERT INTO blockchain_bridge_token (network_id, symbol, contract_address) VALUES (1, 'eXRP', '0x');
+INSERT INTO blockchain_bridge_token (network_id, symbol, contract_address) VALUES (2, 'XRP', '0x');
