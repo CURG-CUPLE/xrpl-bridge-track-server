b3595a8674dd0b14c3d406a98a1fbe075b27e8a8
cysong <cysong@coinone.com>
2024-07-14 15:51:55 +0900

    chore: docker-compose.yaml 수정

--- .container/mysql/initdb.d/1-create_table.sql
+++ .container/mysql/initdb.d/1-create_table.sql
@@ -3,31 +3,30 @@ create table blockchain_network
     id           int unsigned auto_increment
         primary key,
     network      varchar(32)                         not null comment '블록체인 네트워크 이름, ex.Ethereum',
-    block_number bigint unsigned                     not null comment '수집이 완료된 블록 번호',
-    created_at   timestamp default CURRENT_TIMESTAMP not null comment '생성일시'
-);
-
-create table blockchain_bridge_token
-(
-    id               int unsigned auto_increment
-        primary key,
-    network_id       int unsigned                        not null comment 'blockchain_network id',
-    symbol           varchar(32)                         not null comment '코인 심볼, ex.ETH',
-    contract_address varchar(128)                        not null comment 'Bridge Contract Address',
-    created_at       timestamp default CURRENT_TIMESTAMP not null comment '생성일시'
-);
+    block_number bigint unsigned not null comment '수집이 완료된 블록 번호',
+    created_at   timestamp default CURRENT_TIMESTAMP not null comment '생성일시',
+    constraint uk_blockchain_network_network
+        unique (network)
+)   engine = InnoDB
+    charset = utf8mb4
+    comment '블록체인 브릿지 네트워크 정보';
 
 create table blockchain_bridge_transaction
 (
     id           bigint auto_increment
         primary key,
-    token_id     int unsigned                        not null comment 'blockchain_network id',
+    network_id   int unsigned not null comment 'blockchain_network id',
     txid         varchar(128)                        not null comment 'transaction hash',
-    amount       bigint unsigned                     not null comment 'bridge amount',
-    fee          bigint unsigned                     not null comment 'bridge transaction network fee',
-    block_number bigint unsigned                     not null comment 'block number',
+    amount       bigint unsigned not null comment 'bridge amount',
+    fee          bigint unsigned not null comment 'bridge transaction network fee',
+    block_number bigint unsigned not null comment 'block number',
     `to`         varchar(128)                        not null comment 'bridge to address',
-    block_time   bigint unsigned                     not null comment 'block time',
+    block_time   bigint unsigned not null comment 'block time',
     status       varchar(32)                         not null comment '트랜잭션 처리 상태',
-    created_at   timestamp default CURRENT_TIMESTAMP not null comment '생성일시'
-);
+    created_at   timestamp default CURRENT_TIMESTAMP not null comment '생성일시',
+    constraint fk_blockchain_bridge_transaction_network_id
+        foreign key (network_id) references blockchain_network (id)
+)   engine = InnoDB
+    charset = utf8mb4
+    comment '브릿지 트랜잭션 정보';
+
--- .container/mysql/initdb.d/2-init.sql
+++ .container/mysql/initdb.d/2-init.sql
@@ -1,4 +1,2 @@
 INSERT INTO blockchain_network (id, network, block_number) VALUES (1, 'EVM Sidechain', 0);
 INSERT INTO blockchain_network (id, network, block_number) VALUES (2, 'XRP', 0);
-INSERT INTO blockchain_bridge_token (network_id, symbol, contract_address) VALUES (1, 'eXRP', '0x');
-INSERT INTO blockchain_bridge_token (network_id, symbol, contract_address) VALUES (2, 'XRP', '0x');
